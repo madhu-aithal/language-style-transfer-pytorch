941f38a6ad7ffef712405c4dcb05a4d11cb0a1d2
Madhusudhan Aithal <madhu.aithal23@gmail.com>
2020-02-11 14:39:37 -0700

    Updated trainIters in Model class

--- code/style_transfer.py
+++ code/style_transfer.py
@@ -2,6 +2,14 @@ import os
 from vocab import Vocabulary, build_vocab
 from file_io import load_sent, write_sent
 from options import load_arguments
+import sys
+import time
+import ipdb
+import random
+import _pickle as pickle
+
+from utils import *
+# from nn import *
 
 import numpy as np
 import torch
@@ -49,7 +57,7 @@ class Generator(nn.Module):
     def initHidden(self):
         return torch.zeros(1, 1, self.hidden_size, device=device)
 
-class Model(nn.Module):
+class Model():
     def __init__(self, input_size_enc, hidden_size_enc, 
     hidden_size_gen, output_size_gen, dropout_p):
 
@@ -61,9 +69,10 @@ class Model(nn.Module):
         self.generator1 = Generator(self.hidden_size_gen, self.output_size_gen, self.dropout_p)
         self.generator2 = Generator(self.hidden_size_gen, self.output_size_gen, self.dropout_p)
         self.encoder = Encoder(self.input_size_enc, self.hidden_size_enc, self.dropout_p)
-        self.softmax = torch.nn.softmax(self.output_size_gen)
+        self.softmax = torch.nn.LogSoftmax(self.output_size_gen)
         self.EOS_token = 2
         self.k = 5
+        self.gamma = 0.001
 
     # def forward(self, input):
     #     encoder_hidden = self.encoder.initHidden()
@@ -73,7 +82,7 @@ class Model(nn.Module):
         
             # self.encoder(input, encoder_hidden)
 
-    def train(self, input_tensor, target, enc_optim, gen1_optim, gen2_optim, criterion, gamma):
+    def train(self, input_data, target, enc_optim, gen1_optim, gen2_optim, criterion):
         # , max_length=MAX_LENGTH)
 
         enc = self.encoder
@@ -85,13 +94,14 @@ class Model(nn.Module):
         enc_optim.zero_grad()
         gen1_optim.zero_grad()
 
-        input_length = input_tensor.size(0)
+        input_length = input_data.size(0)
+        print("input_size: ", enc.input_size)
+        print("input_data: ", input_data)
+        input_tensor = enc.embedding(input_data)
 
         encoder_outputs = torch.zeros(input_length, enc.hidden_size, device=device)
         gen1_hid_states = []
         gen2_hid_states = []
-
-        # loss = 0
 
         for ei in range(input_length):
             encoder_output, encoder_hidden = enc(
@@ -100,8 +110,6 @@ class Model(nn.Module):
 
         gen1_input = torch.tensor(torch.cat([target,encoder_outputs[-1]]), device=device)
         gen2_input = torch.tensor(torch.cat([(target+1)%2,encoder_outputs[-1]]), device=device)
-
-        # gen1_input = 
 
         gen1_hidden = torch.cat([target,encoder_outputs])
         gen2_hidden = torch.cat([(target+1)%2,encoder_outputs])
@@ -112,34 +120,42 @@ class Model(nn.Module):
         for i in range(input_length):
             gen1_output, gen1_hidden = gen1(
                 gen1_input, gen1_hidden, encoder_outputs[-1])
+            
+            loss += criterion(gen1_output, input_data[i])
+
             gen1_hid_states.append(gen1_hidden)
             gen1_input = input_tensor[i]  # Teacher forcing
 
-        while np.argmax(gen2_output) != self.EOS_token:
+        # while np.argmax(gen2_output) != self.EOS_token:
+        for i in range(input_length):
             gen2_output, gen2_hidden = gen2(
                 gen2_input, gen2_hidden, encoder_outputs)
             gen2_hid_states.append(gen2_hidden)
             topv, topi = gen2_output.topk(1)
             gen2_input = topi.squeeze().detach()  # detach from history as 
-            gen2_input = self.softmax(gen2_input/gamma)
+            gen2_input = self.softmax(gen2_input/self.gamma)
 
-            # loss += criterion(ge, target_tensor[di])
+            loss += criterion(gen2_output, input_data[i])
+
+            if np.argmax(gen2_output) == self.EOS_token:
+                break
             # if decoder_input.item() == EOS_token:
             #     break
 
-        # loss.backward()
+        loss.backward()
 
-        # encoder_optimizer.step()
-        # decoder_optimizer.step()
+        enc_optim.step()
+        gen1_optim.step()
+        gen2_optim.step()
 
         # return loss.item() / target_length
         return encoder_outputs[-1], gen1_hid_states, gen2_hid_states
 
     def trainIters(self, training_data, print_every=1000, plot_every=100, learning_rate=0.01):
-        # start = time.time()
-        # plot_losses = []
-        # print_loss_total = 0  # Reset every print_every
-        # plot_loss_total = 0  # Reset every plot_every
+        start = time.time()
+        plot_losses = []
+        print_loss_total = 0  # Reset every print_every
+        plot_loss_total = 0  # Reset every plot_every
 
         enc_optim = optim.SGD(self.encoder.parameters(), lr=learning_rate)
         gen1_optim = optim.SGD(self.generator1.parameters(), lr=learning_rate)
@@ -148,24 +164,24 @@ class Model(nn.Module):
         criterion = nn.NLLLoss()
         indices = np.arange(training_data.shape[0])
 
-        for i in range(indices):
-            input_tensor = training_data[i]        
+        for i in range(len(indices)):
+            input_data = training_data[i]        
             # target_tensor = training_pair[1]
 
-            loss = train(input_tensor, enc_optim, gen1_optim, gen2_optim, criterion)
-            # print_loss_total += loss
-            # plot_loss_total += loss
+            latent_z, gen1_hid_states, gen2_hid_states = self.train(input_data, 1, enc_optim, gen1_optim, gen2_optim, criterion)
+            print_loss_total += loss
+            plot_loss_total += loss
 
-            # if iter % print_every == 0:
-            #     print_loss_avg = print_loss_total / print_every
-            #     print_loss_total = 0
-            #     print('%s (%d %d%%) %.4f' % (timeSince(start, iter / n_iters),
-            #                                  iter, iter / n_iters * 100, print_loss_avg))
+            if iter % print_every == 0:
+                print_loss_avg = print_loss_total / print_every
+                print_loss_total = 0
+                print('%s (%d %d%%) %.4f' % (timeSince(start, iter / n_iters),
+                                             iter, iter / n_iters * 100, print_loss_avg))
 
-            # if iter % plot_every == 0:
-            #     plot_loss_avg = plot_loss_total / plot_every
-            #     plot_losses.append(plot_loss_avg)
-            #     plot_loss_total = 0
+            if iter % plot_every == 0:
+                plot_loss_avg = plot_loss_total / plot_every
+                plot_losses.append(plot_loss_avg)
+                plot_loss_total = 0
 
         # showPlot(plot_losses)
 
@@ -173,8 +189,7 @@ def get_model(args, vocab):
     dim_hidden = args.dim_y+args.dim_z    
     model = Model(vocab.dim_emb, dim_hidden, 
     dim_hidden, vocab.size, args.dropout_keep_prob)
-    model.trainIters()
-
+    return model
 
 if __name__ == '__main__':
     args = load_arguments()
@@ -202,5 +217,18 @@ if __name__ == '__main__':
     if args.test:
         test0 = load_sent(args.test + '.0')
         test1 = load_sent(args.test + '.1')
+
+    if args.train:
+        model = get_model(args, vocab)
+        batches, _, _ = get_batches(train0, train1, vocab.word2id,
+            args.batch_size, noisy=True)
+        random.shuffle(batches)
+        
+        for batch in batches:
+            # print(len(batch["enc_inputs"]))
+            batch_input = batch["enc_inputs"]
+            batch_input = torch.tensor(np.array(batch_input))
+            print(batch_input.shape)
+            model.trainIters(batch_input)
 
     # print(train0)
