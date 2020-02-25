Updated the softmax function of generator and fixed some bugs in the model

code/autoencoder.py
@@ -77,27 +77,28 @@ class Model():
         return encoder_outputs[-1]
 
     def predict(self, input_data):
-        if torch.cuda.is_available():
-            input_data = input_data.to(device=self.device)
+        # if torch.cuda.is_available():
+        #     input_data = input_data.to(device=self.device)
 
-        input_length = input_data.shape[0]
-        input_tensor = self.encoder.embedding(input_data)
-        batch_size = input_data.shape[1]
-        encoder_hidden = self.encoder.initHidden(device=self.device)
-        encoder_hidden = encoder_hidden.repeat(1,batch_size,1)
+        # input_length = input_data.shape[0]
+        # input_tensor = self.encoder.embedding(input_data)
+        # batch_size = input_data.shape[1]
+        # encoder_hidden = self.encoder.initHidden(device=self.device)
+        # encoder_hidden = encoder_hidden.repeat(1,batch_size,1)
 
-        encoder_outputs = torch.zeros(input_length, 1, batch_size, self.encoder.hidden_size, device=self.device)
+        # encoder_outputs = torch.zeros(input_length, 1, batch_size, self.encoder.hidden_size, device=self.device)
 
-        for ei in range(input_length):
-            encoder_output, encoder_hidden = self.encoder(
-                input_tensor[ei,:,:], encoder_hidden)
-            encoder_outputs[ei] = encoder_hidden 
+        # for ei in range(input_length):
+        #     encoder_output, encoder_hidden = self.encoder(
+        #         input_tensor[ei,:,:], encoder_hidden)
+        #     encoder_outputs[ei] = encoder_hidden 
 
-        latent_z = encoder_outputs[-1]
+        # latent_z = encoder_outputs[-1]
         
         # latent_z = torch.randn_like(latent_z)
-        gen_input = torch.tensor([self.GO_token], device=self.device)
-        gen_hidden = latent_z
+        gen_input = torch.tensor([input_data[0][0]], device=self.device)
+        # gen_hidden = latent_z
+        gen_hidden = torch.zeros(1,1,self.hidden_size_gen, device = self.device)
 
         outputs = [self.vocab.id2word[gen_input[0]]]
         gen_output = torch.zeros(self.output_size_gen, device=self.device)

code/autoencoder_main.py
@@ -86,6 +86,7 @@ if __name__ == '__main__':
     if args.train:
         model = get_model(args, vocab)
         losses_epochs = []
+        save_models = False
         
         for epoch in range(args.max_epochs):
             random.shuffle(train0)
@@ -115,32 +116,32 @@ if __name__ == '__main__':
                 avg_loss += (loss0+loss1)/2
                 running_loss += avg_loss
                 
-                if i%5 == 0:
-                    torch.save(model, save_model_path)
-
                 i+=1
+            
+            if save_models == True and epoch%20 == 0:
+                torch.save(model, save_model_path+"_epoch"+str(epoch))
             print("Avg Loss: ", avg_loss)
             print("---------\n")
             logger.info("Avg Loss: " + str(avg_loss))
             logger.info("---------\n")
 
-        test_input = ["this place was very good !"]
-        test_input = [val.split() for val in test_input]
+        # test_input = ["this place was very good !"]
+        # test_input = [val.split() for val in test_input]
 
-        test_input_processed = []
-        for list_val in test_input:
-            temp_list = []
-            for val in list_val:
-                temp_list.append(model.vocab.word2id[val])
-            test_input_processed.append(temp_list)
-        print(test_input_processed)
-        print(test_input)
-        test_input_tensor = torch.tensor(test_input_processed)
-        print(model.predict(test_input_tensor.t()))
+        # test_input_processed = []
+        # for list_val in test_input:
+        #     temp_list = []
+        #     for val in list_val:
+        #         temp_list.append(model.vocab.word2id[val])
+        #     test_input_processed.append(temp_list)
+        # print(test_input_processed)
+        # print(test_input)
+        # test_input_tensor = torch.tensor(test_input_processed)
+        # print(model.predict(test_input_tensor.t()))
         # writer.add_graph(model)
         # writer.close()
         # model.train_max_epochs(args, train0, train1, vocab, no_of_epochs, save_model_path)
         
-        # torch.save(model, save_model_path)
+        torch.save(model, save_model_path)
 
         

code/model.py
@@ -38,7 +38,7 @@ class Model():
         self.discriminator1 = Discriminator(self.hidden_size_gen, 1).to(self.device)
         self.discriminator2 = Discriminator(self.hidden_size_gen, 1).to(self.device)
 
-        self.softmax = torch.nn.LogSoftmax(self.output_size_gen)
+        self.softmax = torch.nn.LogSoftmax(dim=0)
         self.EOS_token = 2
         self.GO_token = 1
         self.k = 5
@@ -89,10 +89,13 @@ class Model():
         loss = 0
         losses = torch.zeros(input_length, batch_size, device=self.device)
         gen_hid_states = torch.zeros(input_length, gen_hidden.shape[0], gen_hidden.shape[1], gen_hidden.shape[2], device=self.device)
+
+        gen_input = gen_input.unsqueeze(0)
+        gen_input = gen_input.unsqueeze(2)
+        gen_input = self.encoder.embedding(gen_input).squeeze(2)
+
         for i in range(input_length):
-            gen_input = gen_input.unsqueeze(0)
-            gen_input = gen_input.unsqueeze(2)
-            gen_input = self.encoder.embedding(gen_input).squeeze(2)
+            
             gen_output, gen_hidden = gen(
                 gen_input, gen_hidden)
             # loss += criterion(gen_output, true_outputs[:,i])
@@ -102,11 +105,24 @@ class Model():
 
             if teacher_forcing == True:
                 gen_input = true_outputs[:,i]
+                gen_input = gen_input.unsqueeze(0)
+                gen_input = gen_input.unsqueeze(2)
+                gen_input = self.encoder.embedding(gen_input).squeeze(2)
+                # print()
             else:
-                gen_input = torch.argmax(gen_output, dim=1)
+                # gen_input = torch.argmax(gen_output, dim=1)
+                gen_input = self.softmax(gen_output/self.gamma)
+                gen_input = gen_input.unsqueeze(0)
+                # gen_input = gen_input.unsqueeze(2)
+                # print(self.encoder.embedding.weight)
+                gen_input = torch.matmul(gen_input, self.encoder.embedding.weight)
+                # gen_input = self.encoder.embedding(gen_input).squeeze(2)
+                # print()
+
+
                 # topv, topi = gen_output.topk(1)
                 # gen_input = topi.squeeze().detach()
-
+            
             # if torch.argmax(gen_output) == self.EOS_token:
             #     # print("EOS token generated early - generator1")
             #     # self.logger.info("EOS token generated early - generator1")