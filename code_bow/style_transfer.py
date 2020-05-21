1. Added back code_bpe directory to the project (in the previous commit, it was removed for some testing) 2. Fixed a bug related to vocabulary computation - vocab.py was computing character level vocabulary instead of word level

code_bow/style_transfer.py
@@ -15,7 +15,7 @@ import torch.nn as nn
 from torch import optim
 import torch.nn.functional as F
 from pathlib import Path
-import sentencepiece as spm
+# import sentencepiece as spm
 from model import Model
 from torch.utils.tensorboard import SummaryWriter
 import re